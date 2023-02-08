72c2aae3ec1f94b43dae0fa488b266518e215cf3
PhotikLed <ledlightsuper@yandex.ru>
2023-02-08 18:25:29 +0500

    wqef

--- cal.py
+++ cal.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Заготовка')
+
+
+@app.route(' /training/<prof>')
+def training(prof):
+    return render_template('training.html', prof)
 
 
 if __name__ == '__main__':
