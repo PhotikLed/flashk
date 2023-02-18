c602a99448d306ecae032d96fad927a414bb9607
PhotikLed <ledlightsuper@yandex.ru>
2023-02-18 17:56:12 +0500

    wefew

--- cal.py
+++ cal.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+
+from wtf_cal import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -9,9 +12,49 @@ def index():
     return render_template('index.html', title='Заготовка')
 
 
-@app.route(' /training/<prof>')
+@app.route('/training/<prof>')
 def training(prof):
-    return render_template('training.html', prof)
+    return render_template('training.html', prof=prof)
+
+
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    prof = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог',
+            'врач', 'инженер по терраформированию', 'климатолог',
+            'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+            'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода',
+            'киберинженер', 'штурман', 'пилот дронов']
+    return render_template('parasha.html', prof=prof, list=list)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    params = {
+        'title': 'Анкета',
+        'surname': 'Масло',
+        'name': 'Сергей',
+        'education': 'МиМК',
+        'profession': 'мастер по автомобильным маслам',
+        'sex': 'м',
+        'motivation': 'всегда мечтал менять масло в транспорте на Марсе',
+        'ready': 'всегда'
+
+    }
+    return render_template('maslo.html', **params)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def success():
+    return 'SUCCESS'
 
 
 if __name__ == '__main__':
